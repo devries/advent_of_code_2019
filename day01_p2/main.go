819356615594d60eba6fbe6ce1cb0da73e1142b3
Christopher De Vries <devries@idolstarastronomer.com>
2019-12-01 08:59:05 -0500

    Initial advent of code

---
+++ day01_p2/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var input_filename string = "input.txt"
+
+func main() {
+	f, err := os.Open(input_filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	fuelSum := 0
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		var mass int
+		_, err := fmt.Sscanf(scanner.Text(), "%d", &mass)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+			os.Exit(1)
+		}
+
+		for {
+			fuel := moduleFuel(mass)
+			if fuel <= 0 {
+				break
+			}
+			fuelSum += fuel
+			mass = fuel
+		}
+	}
+
+	fmt.Printf("Total Fuel: %d\n", fuelSum)
+}
+
+func moduleFuel(m int) int {
+	return m/3 - 2
+}
