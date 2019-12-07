5455ef58dd92b883e92294d4bcead3681cf7e704
Christopher De Vries <devries@idolstarastronomer.com>
2019-12-07 15:17:51 -0500

    Added a little benchmark of one of the tests

--- day07_p2/main_test.go
+++ day07_p2/main_test.go
@@ -31,3 +31,20 @@ func TestExecution(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkAmplifiers(b *testing.B) {
+	program := "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54," +
+		"-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4," +
+		"53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
+	phases := []int{9, 7, 8, 5, 6}
+
+	inputOpcodes, err := ParseProgram(program)
+	if err != nil {
+		b.Errorf("Error parsing input: %s", err)
+	}
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		_ = Amplifiers(inputOpcodes, phases)
+	}
+}
